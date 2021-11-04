f023deeb33b2ceb7aa3d99932b30d21f4b232655
Maxwell Zen <maxwell.zen@gmail.com>
2021-11-04 01:00:23 -0400

    added spacing

--- main.c
+++ main.c
@@ -9,25 +9,32 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
+
 	char dirname[500];
+
 	if (argc>2) printf("WARNING: Only the first command line argument will be used.\n");
 	if (argc>1) strcpy(dirname, argv[1]);
 	else {
 		printf("Enter a directory to scan: ");
 		scanf("%s", dirname);
 	}
+
 	int n = strlen(dirname);
 	if (dirname[n-1] != '/') dirname[n]='/', dirname[n+1]=0;
+
 	struct stat a;
 	struct dirent *entry;
 	long long filesizes;
+
 	DIR *b = opendir(dirname);
 	if (!b) {
 		printf("Directory [%s] not opened successfully\n", dirname);
 		printf("%s\n", strerror(errno));
 		return 0;
 	}
+
 	char dirs[50000] = "Directories:\n", files[50000] = "Regular files:\n", add[50000], fname[500];
+
 	while ((entry = readdir(b))) {
 		if (entry->d_type == 8) {
 			sprintf(fname, "%s%s", dirname, entry->d_name);
