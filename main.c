e41788f14b35a90664558fcae1e819fcf1a2e379
Maxwell Zen <maxwell.zen@gmail.com>
2021-11-04 00:44:06 -0400

    created user interaction

--- main.c
+++ main.c
@@ -6,21 +6,33 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <errno.h>
 
-int main() {
-	char dirname[50] = ".";
+int main(int argc, char *argv[]) {
+	char dirname[50];
+	if (argc>2) printf("WARNING: Only the first command line argument will be used.\n");
+	if (argc>1) strcpy(dirname, argv[1]);
+	else {
+		printf("Enter a directory to scan: ");
+		scanf("%s", dirname);
+	}
+	int n = strlen(dirname);
+	if (dirname[n-1] != '/') dirname[n]='/', dirname[n+1]=0;
 	struct stat a;
 	struct dirent *entry;
 	long long filesizes;
 	DIR *b = opendir(dirname);
 	if (!b) {
-		printf("Directory not opened successfully\n");
+		printf("Directory [%s] not opened successfully\n", dirname);
+		printf("%s\n", strerror(errno));
 		return 0;
 	}
-	char dirs[5000] = "Directories:\n", files[5000] = "Regular files:\n", add[5000];
+	char dirs[5000] = "Directories:\n", files[5000] = "Regular files:\n", add[5000], fname[5000];
 	while ((entry = readdir(b))) {
 		if (entry->d_type == 8) {
-			stat(entry->d_name, &a);
+			strcpy(fname, dirname);
+			strcat(fname, entry->d_name);
+			stat(fname, &a);
 			sprintf(add, "\t%s    %lld Bytes\n", entry->d_name, a.st_size);
 			strcat(files, add);
 			filesizes += a.st_size;
